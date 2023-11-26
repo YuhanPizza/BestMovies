aa5cd6465bd6a27cc2f0c8a8250f67065f4ade66
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2023-11-26 14:44:00 -0500

    testing Repository, theatreRepository, GetByIdAsync, Test Passed!

--- BestMovies.Test/RepositoryTest/TheatreRepositoryTest.cs
+++ BestMovies.Test/RepositoryTest/TheatreRepositoryTest.cs
@@ -19,13 +19,13 @@ namespace BestMovies.Test.RepositoryTest
 		private async Task<ApplicationDbContext> GetDbContext()
 		{
 			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-				.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+				.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) //using InMemoryDatabase for testing purposes 6.0.25
 				.Options;
-			var databaseContext = new ApplicationDbContext(options);
-			databaseContext.Database.EnsureCreated();
-			if(await databaseContext.Theatres.CountAsync() < 0)
+			var databaseContext = new ApplicationDbContext(options);//creating applicationDbContext
+			databaseContext.Database.EnsureCreated(); //returns a bool if it is created
+			if(await databaseContext.Theatres.CountAsync() < 0) //make sures the database is more than 0
 			{
-				for (int i = 0; i < 10; i++)
+				for (int i = 0; i < 10; i++) //so that we dont inject all these manually just for test anyway
 				{
 					databaseContext.Theatres.Add(
 						new Theatre()
@@ -51,7 +51,7 @@ namespace BestMovies.Test.RepositoryTest
 		public async void TheatreRepository_Add_ReturnsBool()
 		{
 			//Arrange
-			var theatre = new Theatre()
+			var theatre = new Theatre() //create a theatre that we are gonna add to our database
 			{
 				Title = "Cineplex Cinema Empress Walk",
 				Image = "https://res.cloudinary.com/dusfrwsg5/image/upload/v1700191021/Cinema_wpvqfd.png",
@@ -64,15 +64,33 @@ namespace BestMovies.Test.RepositoryTest
 					State = "NC"
 				}
 			};
-			var dbContext = await GetDbContext();
+			var dbContext = await GetDbContext(); //use the private function we created
+			dbContext.Theatres.AsNoTracking();// if you get tracking issues 
 			var theatreRepository = new TheatreRepository(dbContext);
 
 			//Act
 			var result = theatreRepository.Add(theatre);
 
 			//ASSERT
+			//fluent assertions
 			result.Should().BeTrue();//test if its returning true
 
+		}
+
+		[Fact]
+		public async void TheatreRepository_GetByIdAsync_ReturnsTheatre() //testing getbyidAsync
+		{
+			//Arrange
+			var id = 1;
+			var dbContext = await GetDbContext(); // we bring in our context
+			var theatreRepository = new TheatreRepository(dbContext); //the constructor requires that we pass in that context
+
+			//Act
+			var result = theatreRepository.GetByIdAsync(id);
+
+			//Assert
+			result.Should().NotBeNull(); //should be null
+			result.Should().BeOfType<Task<Theatre>>(); //type checking since we dont actullay use real data
 		}
 	}
 }
